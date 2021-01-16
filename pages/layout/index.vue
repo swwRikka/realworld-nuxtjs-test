511660568aecfbc49d3e3cf56616d91c091621c4
swwRikka <1464101599@qq.com>
2021-01-16 19:19:01 +0800

    修改测试

--- pages/layout/index.vue
+++ pages/layout/index.vue
@@ -49,7 +49,7 @@
     <!-- 底部 -->
     <footer>
       <div class="container">
-        <a href="/" class="logo-font">conduit</a>
+        <a href="/" class="logo-font">aaaa</a>
         <span class="attribution">
           An interactive learning project from <a href="https://thinkster.io">Thinkster</a>. Code &amp; design licensed under MIT.
         </span>
